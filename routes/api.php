6891c6bdced124cd18dbced5cc3479302c800884
Amila T Kumarasekara <Amilathushara15@gmail.com>
2022-10-01 00:00:27 +0530

    commit for the complete in challenge04

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\groupByOwnersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,5 +30,7 @@ Route::controller(AttendanceController::class)->group(function (){
     Route::get('/show','index');
 
 });
+
+Route::get('/groupbyowners', [groupByOwnersController::class, 'challenge04']);
 
 
